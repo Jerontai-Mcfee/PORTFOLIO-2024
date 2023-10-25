1e38c81155e3c41fe0e2736cc361230a4b409b57
GuiltyGamingJay <110130238+GuiltyGamingJay@users.noreply.github.com>
2023-10-25 18:08:26 -0400

    working on about and skills next

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import "../styles/Home.css";
 import Navbar from '../Components/Navbar';
 import video1 from '../video/video1.mov';
 import webdev from '../helpers/webdev.jpg';
+import webdesign from '../helpers/webdesign.jpg';
+import appdev from '../helpers/appdev.png';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
@@ -20,43 +22,44 @@ function Home() {
 
 <div className='services'>
     
-      <h1 className='service-text' >Services I provide</h1>
+      <h1 className='service-text' >Services I Provide</h1>
 <div className='service-container'>
 
 <figure className="image-buttons">
-     <button className="pbuttons" >Web Development
-     <img className="is-rounded"  src={webdev} style={{ width: "300px", height: "300px", borderRadius: "50%" }} />
-     </button>
+      
+   <img className="web-dev"  src={webdev} style={{ width: "300px", height: "300px", borderRadius: "50%" }} />
+  <h1 className="webdevtext" >Web Development</h1>
+     
      </figure>
-        
-        <div className='web-design-container box '>
-        <p>Web Design</p>
-        
-        </div>
-        
-        <div className='app-development-container box'>
-        <p>App Development</p>
-       </div>
+        <figure>
+        <img className="web-des"  src={webdesign} style={{ width: "300px", height: "300px", borderRadius: "50%" }} />
+  <h1 className="webddestext" >Web Design</h1>
+        </figure>
+        <figure>
+        <img className="app-dev"  src={appdev} style={{ width: "300px", height: "300px", borderRadius: "50%" }} />
+  <h1 className="appdevtext" >App Development</h1>
+        </figure>
         </div>
       
    </div>
 <div>
-  <div className='about' id="about" >
-   <a><h1 className='know-container'>lets Get To Know Me</h1></a> 
+  <div className='about'  >
+   <a><h1 className='know-container'>Lets Get To Know Me</h1></a> 
 
     <div className="about-container">
-      <div className="description-container box">
+      <div className="description-container">
         <p>
          Dynamic and versatile full-stack web developer adept at solving complex problems, 
          providing seamless equipment installations, and delivering exceptional mobile device repairs. 
-         Je’Rontai possesses a deep proficiency in an array of programming languages and cutting-edge technologies, 
-         including HTML, CSS, JavaScript, Flutter, Swift, Node.js, React, SQL, MongoDB, TypeScript, Angular, jQuery, 
-         MERN Stack, and REST APIs. Known for crafting captivating digital experiences, 
+         Je’Rontai possesses a deep proficiency in an array of programming languages and cutting-edge technologies
          Je’Rontai combines technical expertise with a passion for creating user-friendly solutions that leave a lasting impact.
         </p>
       </div>
-      <div className="languages-container box">
-        <h3>Languages I Practice</h3>
+      </div>
+
+
+      <div className="skills-section">
+        <h1 className="languages">Languages I Practice</h1>
         <ul>
           <li>JavaScript</li>
           <li>HTML</li>
@@ -69,7 +72,7 @@ function Home() {
         </ul>
       </div>
     </div>
-    </div>
+    
 </div>
 
 
