b423665e949446022333351a76943fa64904b772
GuiltyGamingJay <110130238+GuiltyGamingJay@users.noreply.github.com>
2023-10-29 14:05:39 -0400

    updating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import  { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Projects from './pages/Projects';
+import Contacts from './pages/Contacts';
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer';
 
@@ -14,7 +16,9 @@ function App() {
   <Router>
     <Navbar/>
     <Routes>
-      <Route path="/" element ={<Home />} /> 
+      <Route path="/" element ={<Home />} />
+      <Route path="/Projects" element={<Projects />} />
+      <Route path="/Contacts" element={<Contacts/>} />
     </Routes>
     <Footer />
   </Router>
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Projects() {
+  return (
+    <div>Projects</div>
+  )
+}
+
+export default Projects
+
