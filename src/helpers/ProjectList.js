ebc8e47cec6ed9e08818e02103dd6c049328e932
jerontai.mcfee <jerontai07@gmail.com>
2024-03-27 01:53:52 -0400

    uploaded more projects

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,6 +2,13 @@ import YugiohQuiz from '../assets/Images/ProjectImages/yugiquiz.png';
 import FoodDrink from '../assets/Images/ProjectImages/FoodandDrinkGenerator.png';
 import WeatherDashboard from '../assets/Images/ProjectImages/WeatherDashboard.png';
 import PcBuilder from '../assets/Images/ProjectImages/PcBuilder.png';
+import ReactPortfolio from '../assets/Images/ProjectImages/ReactPortfolio.png';
+import WorkDayScheduler from '../assets/Images/ProjectImages/WorkDayScheduler.png';
+import SqlEmployeeTracker from '../assets/Images/ProjectImages/SQLEmployeeTracker.png';
+import TechBlog from '../assets/Images/ProjectImages/Techblog.png';
+import EcommerceBackend from '../assets/Images/ProjectImages/EcommerceBackend.png';
+
+
 
 
 export const ProjectList = [
@@ -40,6 +47,52 @@ export const ProjectList = [
         siteLink: "https://pc-builder-website.onrender.com/",
 
     },
+    {
+        name: "React Portfolio",
+        image:ReactPortfolio,
+        skills:"JavaScript, HTML, CSS, React, Bootstrap, Node.js",
+        techlogo: ["JavaScript", "HTML", "CSS", "React","Bootstrap", "Node.js"],
+        githubLink: "https://github.com/Jerontai-Mcfee/React-portfolio",
+        siteLink: "https://react-portfolio-37ia.onrender.com/",
+
+    },
+    {
+        name: "Work-Day-Scheduler",
+        image:WorkDayScheduler,
+        skills:"JavaScript, HTML, CSS, ",
+        techlogo: ["JavaScript", "HTML", "CSS"],
+        githubLink: "https://github.com/Jerontai-Mcfee/Work-Day-Scheduler",
+        siteLink: "https://jerontai-mcfee.github.io/Work-Day-Scheduler/",
+
+    },
+    {
+        name: "SQL Employee Tracker",
+        image:SqlEmployeeTracker,
+        skills:"JavaScript,  Node.js, MySQL",
+        techlogo: ["JavaScript", "HTML", "CSS"],
+        githubLink: "https://github.com/Jerontai-Mcfee/SQL-Employee-Tracker",
+        siteLink: "https://github.com/Jerontai-Mcfee/SQL-Employee-Tracker",
+
+    },
+    {
+        name: "Tech Blog",
+        image:TechBlog,
+        skills:"JavaScript, HTML, CSS, ",
+        techlogo: ["JavaScript", "SQL", "Node.js"],
+        githubLink: "https://github.com/Jerontai-Mcfee/Tech-Blog",
+        siteLink: "https://jerontai-mcfee.github.io/Tech-Blog/",
+
+    },
+    {
+        name: "Ecommerce Backend",
+        image:EcommerceBackend,
+        skills:"JavaScript,  Node.js, MySQL",
+        techlogo: ["JavaScript", "Node.js", "MySQL"],
+        githubLink: "https://github.com/Jerontai-Mcfee/Module-13-Object-Relational-Mapping",
+        siteLink: "https://github.com/Jerontai-Mcfee/Module-13-Object-Relational-Mapping",
+
+    },
+  
   
     
 ];
